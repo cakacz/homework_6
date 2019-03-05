8490b526906db99349328ec9222d60d9dab04ad3
Chloe Kaczvinsky <cak0052@tigermail.auburn.edu>
2019-03-05 11:38:10 -0600

    Added a case construct to print a help message if -h is entered as an argument

--- part_2/aug_position_with_help_Kaczvinsky.sh
+++ part_2/aug_position_with_help_Kaczvinsky.sh
@@ -1,2 +1,5 @@
 #needs to take 3 arguments and print them in reverse order
+case $@ in
+    -h ) echo "A script that takes any three arguments and prints them in reverse order.";;
+esac
 echo "$3 $2 $1"
