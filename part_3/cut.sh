019c729dac44c3d104f885a157a07753effb3d0c
Chloe Kaczvinsky <cak0052@tigermail.auburn.edu>
2019-03-06 13:50:25 -0600

    Fixed cut.sh by reducing it to just a cut statement; should run now

--- part_3/cut.sh
+++ part_3/cut.sh
@@ -2,7 +2,7 @@
 #truncate each descriptor to the left of the first whitespace
 
 cd CDS
-#file=Metabetaeus_minutus_TRI_1_20_2014_NORM.fasta.transdecoder.cds
-cat Metabetaeus_minutus_TRI_1_20_2014_NORM.fasta.transdecoder.cds | cut -d " " -f 1 | awk '{print $1}' >> output
+file=Metabetaeus_minutus_TRI_1_20_2014_NORM.fasta.transdecoder.cds
+cut $file -d " " -f 1 >> output
 #lines >> output
 
